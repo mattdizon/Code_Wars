3dbaced5fda32156607361d89db8a0f89ec56c1f
mdizon <mbdizon94@gmail.com>
2018-08-28 15:40:10 -0400

    Orginization Update

--- Misc Challenges/oldest.rb
+++
@@ -1,6 +0,0 @@
-# return the two oldest/oldest ages within the array of ages passed in.
-def two_oldest_ages(ages)
-    print ages.sort.max(2)
-
-end
-#two_oldest_ages([4,5,2,10,8])
--- Misc Challenges/permutations.rb
+++
@@ -1,5 +0,0 @@
-
-def permutations(string)
-     print string.chars.to_a.permutation.map(&:join)
-end
-permutations("que")
