1ee0a68537e826a74280593a1f4f101abb7b14aa
mdizon <mbdizon94@gmail.com>
2018-08-28 14:26:38 -0400

    find the integer that appears an odd amount of times

---
+++ find_odd_letter.rb
@@ -0,0 +1,21 @@
+
+
+
+# Given an array, find the int that appears an odd number of times.
+#There will always be only one integer that appears an odd number of times.
+
+# input: array with one integer that appears an odd number of times
+# output: the number that appears odd number of times
+# process: read each element and determine how many times it appears
+# use hash to determine how many times integer appears and add 1 to it
+def find_it(arr)
+    odd_hash = Hash.new(0)
+    arr.each do |x|
+        odd_hash[x] += 1
+    end
+    odd_hash.each do |x,y|
+        if y % 2 != 0
+            print "#{x} \n"
+        end
+    end
+end
